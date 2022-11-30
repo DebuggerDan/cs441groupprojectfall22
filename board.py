8fc3c29e0e4e9824c56c9fbde0f2cb71e9f791c0
Evan <elafleur@pdx.edu>
2022-11-29 22:11:55 -0800

    Added basic strucuture for game
    Signed-off-by: Evan <elafleur@pdx.edu>

--- board.py
+++ board.py
@@ -1,3 +1,48 @@
+from random import randint
+
+
+# let_to_num={'A':0,'B':1, 'C':2,'D':3,'E':4,'F':5,'G':6,'H':7}
 class Board:
-    def __init__(self):
-        pass
+    def print_board(board):
+        print(' 1 2 3 4 5 6 7 8 9 0')
+        print(' ********************')
+        row_num=1
+        for row in board:
+            if row_num <= 9:
+                print("%d|%s|" % (row_num, "|".join(row)))
+                row_num +=1
+            else:
+                row_num = 0
+                print("%d|%s|" % (row_num, "|".join(row)))
+
+    def get_ship_location():
+        #Enter the row number between 1 to 10
+        row=input('Please enter a ship row 0-9 ').upper()
+        while row not in '0123456789':
+            print("Please enter a valid row ")
+            row=input('Please enter a ship row 0-9 ')
+        #Enter the Ship column from A TO H
+        column=input('Please enter a ship column 0-9 ').upper()
+        while column not in '0123456789':
+            print("Please enter a valid column ")
+            column=input('Please enter a ship column 0-9 ')
+        # return int(row)-1,let_to_num[column]
+        return int(row)-1, int(column)-1
+
+    #Function that creates the ships
+    def create_ships(board):
+        for ship in range(5):
+            ship_r, ship_cl=randint(0,9), randint(0,9)
+            while board[ship_r][ship_cl] =='X':
+                ship_r, ship_cl = randint(0, 9), randint(0, 9)
+            board[ship_r][ship_cl] = 'X'
+
+    def count_hit_ships(board):
+        count=0
+        for row in board:
+            for column in row:
+                if column=='X':
+                    count+=1
+        return count
+
+    
--- main.py
+++ main.py
@@ -1,2 +1,33 @@
-if __name__ == "__main__":
+from board import Board as bd
+Hidden_Pattern=[[' ']*10 for x in range(10)]
+Guess_Pattern=[[' ']*10 for x in range(10)]
+
+def main():
+    bd.create_ships(Hidden_Pattern)
+    bd.print_board(Hidden_Pattern)
+    turns = 10
+    while (turns > 0):
+        print('Welcome to Battleship')
+        bd.print_board(Guess_Pattern)
+        row,column = bd.get_ship_location()
+        if (Guess_Pattern[row][column] == '-'):
+            print(' You already guessed that ')
+        elif (Hidden_Pattern[row][column] =='X'):
+            print(' Congratulations you have hit the battleship ')
+            Guess_Pattern[row][column] = 'X'
+            turns -= 1
+        else:
+            print('Sorry,You missed')
+            Guess_Pattern[row][column] = '-'
+            turns -= 1
+        if  (bd.count_hit_ships(Guess_Pattern) == 5):
+            print("Congratulations you have sunk all the battleships ")
+            break
+        print(' You have ' +str(turns) + ' turns remaining ')
+        if (turns == 0):
+            print('Game Over ')
+            break
+
+if (__name__ == "__main__"):
+    main()
     pass
