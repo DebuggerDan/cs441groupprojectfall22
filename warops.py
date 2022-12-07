84e9a3665e1f94c79a70374059078fa1bbc1fc83
dan <68046886+DebuggerDan@users.noreply.github.com>
2022-12-06 20:28:32 -0800

    WOBR: War Ops. Battleship Reactor - WarOps.py init

---
+++ warops.py
@@ -0,0 +1,26 @@
+# CS 441, Fall 2022 - Group Project: Battleships - War Operations (Plotting, Strategy, & Miscellaneous Battleship Components) - Alana G., Evan L., & Dan J.
+
+import numpy as np
+import matplotlib
+# import random as rnd # for radnom number added to picture but prolly will just append date-time, if possible
+
+matplotlib.use('Agg')
+
+# Graph Implementation - Takes in two .csv files and plots the data (can be made to just take in two lists of x & y coordinates too {or tuples})
+def sitrep(player1, player2):
+    x1,y1 = np.loadtext(player1, delimiter = ',', unpack=True)
+    x2,y2 = np.loadtext(player2, delimiter = ',', unpack=True)
+    
+    np.plot(x1, y1, label = "Player 1 Results")
+    np.plot(x2, y2, label = "Player 2 Results")
+    
+    np.title('WOBR: Player 1 vs. Player 2')
+    np.xlabel('# [Iteration(s)] of Game(s) Played')
+    np.ylabel('Performance (y-axis)')
+    
+    np.legend()
+    np.show()
+    
+    np.savefig('wobr.png')
+    
+    
