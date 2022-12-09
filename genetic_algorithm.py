f2b81e1557cddb697d38f153bcf34dcc8fa66adb
Alana Gilston <abluescarab@users.noreply.github.com>
2022-12-09 00:33:52 -0800

    Added reference pseudocode from the book and made algorithm methods static

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,3 +1,33 @@
+# function Genetic-Algorithm(population, Fitness-Fn) returns an individual
+#     inputs: population, a set of individuals
+#             Fitness-Fn, a function that measures the fitness of an individual
+#     repeat
+#         new_population <- empty set
+#         for i = 1 to Size(population) do
+#             x <- Random-Selection(population, Fitness-Fn)
+#             y <- Random-Selection(population, Fitness-Fn)
+#             child <- Reproduce(x, y)
+#             if(small random probability) then child <- Mutate(child)
+#             add child to new_population
+#         population <- new_population
+#     until some individual is fit enough, or enough time has elapsed
+#     return the best individual in population, according to Fitness-Fn
+
+# function Reproduce(x, y) returns an individual
+#     inputs: x, y, parent individuals
+#     n <- Length(x)
+#     c <- random number from 1 to n
+#     return Append(Substring(x, 1, c), Substring(y, c + 1, n))
+
 class GeneticAlgorithm:
-    def __init__(self):
+    @staticmethod
+    def _mutate():
+        pass
+
+    @staticmethod
+    def _reproduce():
+        pass
+
+    @staticmethod
+    def run():
         pass
--- hill_climbing.py
+++ hill_climbing.py
@@ -1,3 +1,15 @@
+# function Hill-Climbing(problem) returns a state that is a local maximum
+#     current <- Make-Node(problem.Initial-State)
+#     loop do
+#         neighbor <- a highest-value successor of current
+#         if neighbor.Value <= current.Value then return current.State
+#         current <- neighbor
+
 class HillClimbing:
-    def __init__(self):
+    @staticmethod
+    def _make_node():
+        pass
+
+    @staticmethod
+    def run():
         pass
